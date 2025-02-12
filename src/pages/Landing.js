b83d6980f901f101adb94d997811236749824d8a
Kevin Gill <kgill505@gmail.com>
2025-02-11 17:43:38 -0700

    Images...

---
+++ src/pages/Landing.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Frame } from "../components";
+import kk from "../images/kk.jpeg";
+import cute from "../images/cute.jpeg";
+import hammock from "../images/hammock.jpeg";
+
+export const Landing = () => {
+  return (
+    <Frame>
+      <div className="w-full bg-[#C9E7DF] overflow-hidden h-full">
+        <div className="flex-col justify-between items-center h-full">
+          <div className="pl-2 sm:pl-0 w-full h-1/2 flex justify-start items-center">
+            <img src={hammock} alt="hammock" className="h-[90%]" />
+          </div>
+          <div className="w-full h-1/2 flex items-center">
+            <div className="bg-[#C8E2FD] h-full sm:flex pr-12 items-center justify-end w-1/2 hidden">
+              <img src={kk} alt="kk" className="h-[90%]" />
+            </div>
+            <div className="bg-[#E3D2DA] flex sm:justify-start justify-end sm:pl-12 pr-2 items-center h-full w-full sm:w-1/2">
+              <img src={cute} alt="cute" className="h-[90%]" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </Frame>
+  );
+};
